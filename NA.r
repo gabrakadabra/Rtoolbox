fe2a33232f0031cac128c6a1969b006211337c08
Gabriel Granasen <iphone.granasen@gmail.com>
2011-02-17 09:09:06 +0100

    NA analys tillagd

---
+++ NA.r
@@ -0,0 +1,18 @@
+require(ggplot2)
+
+nna <- function(x, col = TRUE) {
+	if (col) nNA <- colSums(is.na(x))	
+	else nNA <- rowSums(is.na(x))
+
+	tmo2 <- data.frame(nNA)
+	tmo2$nAviable <- (dim(x)[1] - tmo2)[,]
+	print(tmo2)
+	return(nNA)
+}
+
+plotNA <-  function(indata) {
+	data_NA <- is.na(indata)
+	data_NA_m <- melt(data_NA)
+	(p <- ggplot(data_NA_m, aes(X1, X2)) + geom_tile(aes(fill = factor(value)),colour = 'white') + scale_fill_manual('Is NA?',values = alpha(c("blue", "red"), 0.5))) +
+	opts(title = paste('NA in:',opts(title = substitute(indata))))
+}
